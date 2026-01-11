d5b8f12d5f0f15fc0fb1c29b018442dd5a718958
kfir7006 <kfir.wein@gmail.com>
2026-01-11 14:42:23 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, get, onValue, remove } from 'firebase/database';
-
 const firebaseConfig = {
   apiKey: "AIzaSyAxhDvJnlB9QHxCNKsPFPGEyRabaLNZeDU",
   authDomain: "pokedescribe-game.firebaseapp.com",
-  databaseURL: "https://pokedescribe-game-default-rtdb.firebaseio.com",
+  databaseURL: "https://pokedescribe-game-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "pokedescribe-game",
   storageBucket: "pokedescribe-game.firebasestorage.app",
   messagingSenderId: "8250502072",
   appId: "1:8250502072:web:086a7ea583fb14ba067d4d"
 };
-
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
-export { ref, set, get, onValue, remove };
